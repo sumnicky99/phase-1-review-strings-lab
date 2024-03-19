84b77306b62985a52e1ebcdd7131e8a8a171f176
Sumnicky99 <sumnicky99@gmail.com>
2024-03-19 13:34:49 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Sumaya Mohamed';
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!';
+const shortGreeting = 'Welcome, ' + currentUser.charAt(0) + '!';
